9d7269749ecb536fa874052b99f0f6d0aa3a6a0e
ragnar <rahulgovind517@gmail.com>
2017-03-25 19:39:18 +0530

    Graphs added to the analysis page

--- server/flaskr/flaskr.py
+++ server/flaskr/flaskr.py
@@ -5,18 +5,22 @@ from flask import request, session
 from middleware.StreamConsumingMiddleware import StreamConsumingMiddleware
 from models.models import db, Profile
 from werkzeug.utils import secure_filename
-from numpy.random import randint
+from numpy.random import randint, rand
+import json
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
 UPLOAD_FOLDER = '/tmp'
 
 
 def create_app(migrate=False):
     app = Flask(__name__)  # create the application instance :)
     app.wsgi_app = StreamConsumingMiddleware(app.wsgi_app)
-    db.init_app(app)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     if migrate:
         with app.app_context():
@@ -38,10 +42,48 @@ def get_session_files_key(key):
     return 'files-' + key
 
 
+def main_score(profile_dict, resume_file_name):
+    result = {}
+    for key in profile_dict.keys():
+        result[key] = rand()
+    return result
+
+
+def get_profile_dict():
+    profiles = Profile.query.all()
+    result = {}
+    for profile in profiles:
+        result[profile.name] = profile.keyword_list
+    return result
+
+
+def get_analysis(resume_file_name):
+    profile_dict = get_profile_dict()
+    result = main_score(profile_dict, resume_file_name)
+    return result
+
+
+def get_analyses(resume_file_names):
+    id = 1
+    result = []
+    for file_name in resume_file_names:
+        result_here = {
+            'id': id,
+            'scores': get_analysis(file_name)
+        }
+
+        result_here['score_keys'] = json.dumps(result_here['scores'].keys())
+        result_here['score_values'] = json.dumps(result_here['scores'].values())
+        id += 1
+        result.append(result_here)
+
+    return result
+
+
 @app.route('/', methods=['GET', 'POST'])
 def show_hello_world():
     if request.method == 'GET':
-        return render_template("home.html", file_session=randint(1, 1e9))
+        return render_template("home.html", file_session=randint(1, int(1e9)))
     else:
         files = request.files.getlist('files[]')
         file_paths = []
@@ -52,7 +94,7 @@ def show_hello_world():
             print 'Filename: ', file.filename
             if len(file.filename) > 0:
                 filename = secure_filename(file.filename)
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_session_key + filename)
                 print file_path
 
                 file.save(file_path)
@@ -76,9 +118,10 @@ def show_hello_world():
 @app.route('/analyze')
 def analyze():
     file_session_key = request.args.get('file_session')
+    file_names = session[get_session_files_key(file_session_key)]
     if get_session_files_key(file_session_key) in session:
         files = session[get_session_files_key(file_session_key)]
-        return render_template("show-analysis.html", files=files)
+        return render_template("show-analysis.html", files=files, analyses=get_analyses(file_names))
 
 
 @app.route('/create-profile', methods=['GET', 'POST'])
--- server/flaskr/models/models.py
+++ server/flaskr/models/models.py
@@ -3,11 +3,10 @@ import re
 
 db = SQLAlchemy()
 
-
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
-    keywords = db.Column(db.String(120), unique=True)
+    keywords = db.Column(db.Text)
 
     def __init__(self, name, keywords):
         self.name = name
